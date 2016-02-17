f133cf6a8ec4d16012f70c2543a5688646e28174
shelleywitte <Student@SHC-380-010.shc.wisc.edu>
2016-02-17 17:09:05 -0600

    proportional symbols; working on popups

--- js/geojson.js
+++ js/geojson.js
@@ -16,45 +16,64 @@ function createMap(){
     getData(map);
 };
 
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 50;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
 
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
+    return radius;
 };
+
+function pointToLayer(feature, latlng){
+
+    var attribute = "Pop_2015";
+
+    var options = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    var attValue = Number(feature.properties[attribute]);
+    options.radius = calcPropRadius(attValue)
+
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+    layer.bindPopup(popupContent)
+
+    return layer;
+};
+
+function createPropSymbols(data, map){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
+
+
+
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
     $.ajax("data/MegaCities.geojson", {
         dataType: "json",
         success: function(response){
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-
-            // L.geoJson(response, {
-            //     onEachFeature: onEachFeature
-            // }).addTo(map);
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
         }
     });
 };
